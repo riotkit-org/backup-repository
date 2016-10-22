77344769903abe47ab7000567ec934c1b0f4112e
Black and Red <wesoly.krzysztofa@gmail.com>
2016-10-22 08:50:52 +0200

    Deployment script

--- bin/deploy-to-ftp.sh
+++ bin/deploy-to-ftp.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ ! -f "./composer.json" ]];
+then
+    echo "Please run this script from the main directory of the application"
+    exit 1
+fi
+
+git rev-list --all --count > ./var/version-number
 composer install
 
 echo -n "Host: "htaccess for Apache, a few new controllers :)
