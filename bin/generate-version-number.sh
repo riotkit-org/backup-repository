5a74e314702fe9bb22c54e191d368cbed911285c
Andrew Johnson <blackandred@users.noreply.github.com>
2016-11-05 14:40:57 +0100

    Fixed heroku integration

--- bin/generate-version-number.sh
+++ bin/generate-version-number.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [[ ! -f "./composer.json" ]];
-then
+if [[ ! -f "./composer.json" ]]; then
     echo "Please run this script from the main directory of the application"
     exit 1
 fi
 
+if [[ ! -d .git ]]; then
+    echo "Cannot generate a version number, as not in a git repository"
+    exit 0
+fi
+
 git rev-list --all --count > ./var/version-number
+exit $?
