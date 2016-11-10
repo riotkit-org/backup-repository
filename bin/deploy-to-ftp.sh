b9f01fc418f2bfb7e62302c0b8e0261c86f8cf96
Black and Red <wesoly.krzysztofa@gmail.com>
2016-11-10 07:43:54 +0100

    Fixed last commit

--- bin/deploy-to-ftp.sh
+++ bin/deploy-to-ftp.sh
@@ -19,12 +19,15 @@ composer install
 echo "==> Clearing up dev cache"
 rm -rf ./var/cache/profiler/
 rm ./var/cache/logs/silex_dev.log
+rm ./config/database.sqlite3
 
 echo "==> Uploading all files"
 ncftpput -u $DEPLOYMENT_FTP_USER -R -v -p $DEPLOYMENT_FTP_PASSWD $DEPLOYMENT_FTP_SERVER $DEPLOYMENT_FTP_DIR ./
 
 echo "==> Removing files that should not be deployed to server"
-for fileName in $(cat ./.gitignore); do
+for fileName in $(cat ./.production-ignore); do
+    echo "~ rm $DEPLOYMENT_FTP_DIR/$fileName"
+
     ncftp -u $DEPLOYMENT_FTP_USER $DEPLOYMENT_FTP_SERVER $DEPLOYMENT_FTP_SERVER -p $DEPLOYMENT_FTP_PASSWD <<END_OF_CMD
 rm $DEPLOYMENT_FTP_DIR/$fileName
 quit
