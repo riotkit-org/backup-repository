234fed7481fa9130da915d90a58963081d85d70c
Black and Red <wesoly.krzysztofa@gmail.com>
2016-11-18 07:18:44 +0100

    Changed deployment method to phploy, which seems to be working very well

--- bin/deploy-to-ftp.sh
+++ bin/deploy-to-ftp.sh
@@ -6,30 +6,23 @@ then
     exit 1
 fi
 
-if [[ ! -f "./config/DEPLOYMENT_FTP" ]];
+if [[ ! -f "./phploy.ini" ]];
 then
-    echo "You need to create a config/DEPLOYMENT_FTP file from the template"
+    echo "You need to create a phploy.ini file from the template"
     exit 1
 fi
 
-source ./config/DEPLOYMENT_FTP
-./bin/generate-version-number.sh
-composer install
+command=""
 
-echo "==> Clearing up dev cache"
-rm -rf ./var/cache/profiler/
-rm ./var/cache/logs/silex_dev.log
-rm ./config/database_prod.sqlite3 2>/dev/null
+if type phploy >/dev/null 2>&1; then
+    command="phploy"
+elif [[ -f ~/.composer/vendor/banago/phploy/bin/phploy.phar ]]; then
+    command="~/.composer/vendor/banago/phploy/bin/phploy.phar"
+fi
 
-echo "==> Uploading all files"
-ncftpput -u $DEPLOYMENT_FTP_USER -R -v -p $DEPLOYMENT_FTP_PASSWD $DEPLOYMENT_FTP_SERVER $DEPLOYMENT_FTP_DIR ./
+if [[ ! $command ]]; then
+    echo "Cannot find a phploy installed, you can install it via composer globally"
+    echo "See: https://github.com/banago/PHPloy"
+fi
 
-echo "==> Removing files that should not be deployed to server"
-for fileName in $(cat ./.production-ignore); do
-    echo "~ rm $DEPLOYMENT_FTP_DIR/$fileName"
-
-    ncftp -u $DEPLOYMENT_FTP_USER $DEPLOYMENT_FTP_SERVER $DEPLOYMENT_FTP_SERVER -p $DEPLOYMENT_FTP_PASSWD <<END_OF_CMD
-rm $DEPLOYMENT_FTP_DIR/$fileName
-quit
-END_OF_CMD
-done
+eval $command
