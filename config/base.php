fdc0e91bc026775f311cf706f8dac46951f4d92a
Andrew Johnson <blackandred@users.noreply.github.com>
2017-05-09 10:51:10 +0200

    Added integration with Heroku

---
+++ config/base.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+function getConfigurationValue(string $name, $default, $allowEmptyString = false, $isJson = false)
+{
+    $value = $default;
+
+    if (array_key_exists($name, $_SERVER)) {
+        $value = $_SERVER[$name];
+    }
+
+    if ($value === '' && !$allowEmptyString) {
+        $value = null;
+
+    } elseif ($value == 'null') {
+        $value = null;
+
+    } elseif ($isJson === true) {
+        $value = json_decode($value, true);
+    }
+
+    return $value;
+}
