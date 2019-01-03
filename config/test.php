ae17251e95f0f7f2d83e91246519bb0500b42a67
Andrew Johnson <blackandred@users.noreply.github.com>
2019-01-03 14:08:09 +0100

    Deleted first version

--- config/test.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-$app = require __DIR__ . '/dev.php';
-
-if (is_file(__DIR__ . '/test.custom.php')) {
-    require __DIR__ . '/test.custom.php';
-}
-
-return $app;
--- src/Exception/ImageManager/ImageManagerException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Exception\ImageManager;
-
-class ImageManagerException extends \Exception { };
