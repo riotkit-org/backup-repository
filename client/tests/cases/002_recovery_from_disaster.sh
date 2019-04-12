b67c5fb80ed9deff4cc3cd395876c0fa16b0fdba
Andrew Johnson <blackandred@users.noreply.github.com>
2019-04-12 07:08:54 +0200

    Travis: Fixed Bahub functional build

--- client/tests/cases/002_recovery_from_disaster.sh
+++ client/tests/cases/002_recovery_from_disaster.sh
@@ -9,7 +9,7 @@ ADMIN_TOKEN=$(generate_admin_token)
 echo " >> Token: ${ADMIN_TOKEN}"
 
 # clean up
-rm -rf /tmp/bash.restored
+rm -rf /tmp/fstab.restored
 
 COLLECTION_ID=$(create_collection "Logs" "logs.tar.gz" 4 3GB 14GB)
 
@@ -19,7 +19,7 @@ bahub --config /etc/bahub/without_crypto.conf.yaml --debug backup local_command_
 echo " >> Executing recovery"
 bahub --config /etc/bahub/without_crypto.conf.yaml --debug recover plan_2
 
-if sudo docker exec tests_bahub_1 strings /tmp/bash.restored|grep bash > /dev/null; then
+if sudo docker exec tests_bahub_1 cat /tmp/fstab.restored|grep "/"; then
     echo " >> Expected to recover a file [ OK ]"
     exit 0
 fi
