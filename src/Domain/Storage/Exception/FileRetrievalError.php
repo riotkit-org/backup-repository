33b84a3acf887c232a7e023dddf21a330d008738
Andrew Johnson <blackandred@users.noreply.github.com>
2019-07-07 10:54:25 +0200

    Server: Handling of cURL errors when downloading a remote URL. Now the error is now shown as 500, but as a 400 with details

--- src/Domain/Storage/Exception/FileRetrievalError.php
+++ src/Domain/Storage/Exception/FileRetrievalError.php
@@ -7,4 +7,7 @@ class FileRetrievalError extends StorageException
     public const CODE_UPLOAD_MAX_FILESIZE = 60001;
     public const CODE_POST_MAX_FILESIZE   = 60002;
     public const EMPTY_REQUEST            = 60003;
+    public const URL_NOT_FOUND            = 60004;
+    public const URL_SERVER_NOT_REACHABLE = 60005;
+    public const URL_GENERAL_ERROR        = 60006;
 }
--- src/Infrastructure/Storage/Manager/DownloadViaGuzzleProvider.php
+++ src/Infrastructure/Storage/Manager/DownloadViaGuzzleProvider.php
@@ -3,10 +3,12 @@
 namespace App\Infrastructure\Storage\Manager;
 
 use App\Domain\Common\Manager\StateManager;
+use App\Domain\Storage\Exception\FileRetrievalError;
 use App\Domain\Storage\Provider\HttpDownloadProvider;
 use App\Domain\Storage\ValueObject\Stream;
 use App\Domain\Storage\ValueObject\Url;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
 
 class DownloadViaGuzzleProvider implements HttpDownloadProvider
 {
@@ -22,6 +24,27 @@ class DownloadViaGuzzleProvider implements HttpDownloadProvider
 
     public function getStreamFromUrl(Url $url): Stream
     {
-        return new Stream($this->client->get($url->getValue())->getBody()->detach());
+        try {
+            return new Stream($this->client->get($url->getValue())->getBody()->detach());
+
+        } catch (ConnectException $exception) {
+            $msg = $exception->getMessage();
+
+            if (stripos($msg, 'Could not resolve host') !== false
+                || stripos($msg, 'cURL error 6') !== false
+                || stripos($msg, 'Resolving timed out') !== false
+                || stripos($msg, 'cURL error 28') !== false) {
+                throw new FileRetrievalError('URL is not resolvable, the domain is not valid',
+                    FileRetrievalError::URL_SERVER_NOT_REACHABLE);
+            }
+
+            if (stripos($msg, 'Not found') !== false) {
+                throw new FileRetrievalError('URL is not valid, file not found',
+                    FileRetrievalError::URL_NOT_FOUND);
+            }
+
+            throw new FileRetrievalError('Cannot access remote URL, unrecognized reason: ' . $msg,
+                FileRetrievalError::URL_GENERAL_ERROR);
+        }
     }
 }
