03834f6b5395499a228b53790ca2a8927982443e
Andrew Johnson <blackandred@users.noreply.github.com>
2019-08-10 07:43:56 +0200

    CI: Corrected notifications

--- .infrastructure/notify.sh
+++ .infrastructure/notify.sh
@@ -2,6 +2,8 @@
 
 SLACK_URL=$1
 MESSAGE=$2
+MESSAGE=${MESSAGE/\[OK\]/:white_check_mark:}
+MESSAGE=${MESSAGE/\[FAILURE\]/:exclamation:}
 
 [[ "$SLACK_URL" ]] && echo "{\"text\": \"[Travis-CI] ${MESSAGE}\"}" | curl -H 'Content-type: application/json' -d @- -X POST -s "${SLACK_URL}";
 
