706cfb856f8abec1f92ba577f7b841513afa21bb
Andrew Johnson <blackandred@users.noreply.github.com>
2020-01-03 22:54:32 +0100

    Added `storage:file:read` command

---
+++ server/src/Command/Storage/ReadFileCommand.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace App\Command\Storage;
+
+use App\Domain\Storage\ActionHandler\ViewFileHandler;
+use App\Domain\Storage\Context\CachingContext;
+use App\Domain\Storage\Factory\Context\SecurityContextFactory;
+use App\Domain\Storage\Form\ViewFileForm;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Reads a file from the storage
+ */
+class ReadFileCommand extends Command
+{
+    public const NAME = 'storage:file:read';
+
+    /**
+     * @var ViewFileHandler
+     */
+    private $handler;
+
+    /**
+     * @var SecurityContextFactory
+     */
+    private $contextFactory;
+
+    public function __construct(ViewFileHandler $handler, SecurityContextFactory $contextFactory)
+    {
+        $this->handler        = $handler;
+        $this->contextFactory = $contextFactory;
+
+        parent::__construct(self::NAME);
+    }
+
+    protected function configure()
+    {
+        $this->setName(self::NAME)
+            ->setDescription('Read the file from storage and print out to the stdout')
+            ->addArgument('filename', InputOption::VALUE_REQUIRED,'Filename');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $form = new ViewFileForm();
+        $form->filename   = $input->getArgument('filename');
+        $form->bytesRange = '';
+
+        $cachingContext = new CachingContext('');
+        $securityContext = $this->contextFactory->createReadContextInShell();
+
+        $response = $this->handler->handle($form, $securityContext, $cachingContext);
+        $callback = $response->getResponseCallback();
+
+        if (!$callback) {
+            throw new \Exception('Cannot read the file, maybe the id is not valid?');
+        }
+
+        $callback();
+    }
+}
