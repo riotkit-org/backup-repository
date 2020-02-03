5cd940b694bd3b805d4f7b77e6b30a089aac3054
Andrew Johnson <blackandred@users.noreply.github.com>
2020-02-03 07:11:02 +0100

    LCM: Initial Dockerfile

--- little-copy-machine-client/setup.py
+++ little-copy-machine-client/setup.py
@@ -2,18 +2,26 @@ from setuptools import setup, find_packages
 import yaml
 import os
 
-version = '?'
+version_yaml_path = 'unknown'
+version_yaml_paths = [
+    os.path.dirname(os.path.realpath(__file__)) + '/../server/config/version.yaml',
+    '/build/version.yaml'
+]
 
-with open(os.path.dirname(os.path.realpath(__file__)) + '/../server/config/version.yaml', 'rb') as yamlFile:
-    asDict = yaml.safe_load(yamlFile)
-    version = asDict['version']
+for path in version_yaml_paths:
+    if os.path.isfile(path):
+        version_yaml_path = path
 
-setup(
-    name="File Repository Little Copy Machine",
-    version=version,
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    entry_points={
-        'console_scripts': ['rkt-lcm=riotkit.filerepository.littlecopymachine:main'],
-    }, install_requires=['requests>=2.20', 'sqlalchemy>=1.3']
-)
+with open(version_yaml_path, 'rb') as yaml_content:
+    as_dict = yaml.safe_load(yaml_content)
+    version = as_dict['version']
+
+    setup(
+        name="File Repository Little Copy Machine",
+        version=version,
+        package_dir={'': 'src'},
+        packages=find_packages(where='src'),
+        entry_points={
+            'console_scripts': ['rkt-lcm=riotkit.filerepository.littlecopymachine:main'],
+        }, install_requires=['requests>=2.20', 'sqlalchemy>=1.3']
+    )
