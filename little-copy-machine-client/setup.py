261deece71bd42d99721b989fd0f56cc94507699
Andrew Johnson <blackandred@users.noreply.github.com>
2020-02-07 07:25:28 +0100

    LCM: Migrated to PBR

--- little-copy-machine-client/setup.py
+++ little-copy-machine-client/setup.py
@@ -1,27 +1,10 @@
+#!/usr/bin/env python3
+
 from setuptools import setup, find_packages
-import yaml
-import os
 
-version_yaml_path = 'unknown'
-version_yaml_paths = [
-    os.path.dirname(os.path.realpath(__file__)) + '/../server/config/version.yaml',
-    '/build/version.yaml'
-]
-
-for path in version_yaml_paths:
-    if os.path.isfile(path):
-        version_yaml_path = path
-
-with open(version_yaml_path, 'rb') as yaml_content:
-    as_dict = yaml.safe_load(yaml_content)
-    version = as_dict['version']
-
-    setup(
-        name="File Repository Little Copy Machine",
-        version=version,
-        package_dir={'': 'src'},
-        packages=find_packages(where='src'),
-        entry_points={
-            'console_scripts': ['rkt-lcm=riotkit.filerepository.littlecopymachine:main'],
-        }, install_requires=['requests>=2.20', 'sqlalchemy>=1.3']
-    )
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+    package_dir={'': 'src'},
+    packages=find_packages(where='src')
+)
