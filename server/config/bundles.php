58627bf44961993a40cb59a7c7911664802ada8e
Andrew Johnson <blackandred@users.noreply.github.com>
2020-03-07 00:17:09 +0100

    Server: Fix codeception support

--- server/config/bundles.php
+++ server/config/bundles.php
@@ -1,4 +1,6 @@
-<?php
+<?php declare(strict_types=1);
+
+use Sentry\SentryBundle\SentryBundle;
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
@@ -12,4 +14,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    SentryBundle::class => ['prod']
 ];
