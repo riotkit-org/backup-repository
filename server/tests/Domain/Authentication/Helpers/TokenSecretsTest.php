dc566cd9bed7f4d0e16921058375a4f0f61926ad
Andrew Johnson <blackandred@users.noreply.github.com>
2020-03-16 06:35:23 +0100

    Server: Resolve problem with unit test on fresh database

--- server/tests/Domain/Authentication/Helpers/TokenSecretsTest.php
+++ server/tests/Domain/Authentication/Helpers/TokenSecretsTest.php
@@ -2,11 +2,11 @@
 
 namespace Tests\Domain\Authentication\Entity;
 
+use App\Domain\Authentication\Entity\Token;
 use App\Domain\Authentication\Helper\TokenSecrets;
 use App\Infrastructure\Authentication\Repository\TokenDoctrineRepository;
 use Doctrine\ORM\AbstractQuery;
-use Doctrine\ORM\Cache;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Ramsey\Uuid\Uuid;
 use Tests\BaseTestCase;
 
 class TokenSecretsTest extends BaseTestCase
@@ -25,14 +25,27 @@ class TokenSecretsTest extends BaseTestCase
         self::bootKernel();
         $repository = self::$container->get(TokenDoctrineRepository::class);
 
+        // we need to have any data in the table in order to execute any query...
+        // even if we want to only execute something like SELECT 1. The Doctrine does not allow to delete "FROM".
+        $this->populateDatabaseWithExampleToken($repository);
+
         // pass our DQL through Doctrine to see if it is correct
         $qb = $repository->createQueryBuilder('t');
         $qb->select($generated);
+
         $parsedResult = $qb->getQuery()->getResult(AbstractQuery::HYDRATE_SCALAR);
 
         // assertions
         $this->assertSame($strippedInPHP, $parsedResult[0][1], 'UUIDv4 should be "censored" same way in Doctrine as in PHP');
         $this->assertSame(strlen($uuid), strlen($parsedResult[0][1]), 'Censored UUIDv4 and original UUIDv4 should have same length');
         $this->assertSame('*****f40-**87-**7c-**bb-********e8c0', $parsedResult[0][1], 'The result should be predictable');
+    }
+
+    private function populateDatabaseWithExampleToken(TokenDoctrineRepository $repository): void
+    {
+        $testToken = new Token();
+        $testToken->setId(Uuid::uuid4()->toString());
+        $repository->persist($testToken);
+        $repository->flush($testToken);
     }
 }
