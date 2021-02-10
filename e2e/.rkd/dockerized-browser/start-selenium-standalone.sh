df2222de53e197d6d3e0d8fc6a85fcd5235906cb
Andrew Johnson <blackandred@users.noreply.github.com>
2021-02-10 23:11:08 +0100

    E2E: Preparation for running on CI - a ready, working container with browser

--- e2e/.rkd/dockerized-browser/start-selenium-standalone.sh
+++ e2e/.rkd/dockerized-browser/start-selenium-standalone.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-exec google-chrome --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222
+# workaround: --remote-debugging-address=0.0.0.0 does not work in Google Chrome
+sudo apt-get update
+sudo apt-get install socat -y
+socat tcp4-listen:9223,reuseaddr,fork,bind=0.0.0.0 tcp:127.0.0.1:9222 &
+
+exec google-chrome --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --no-default-browser-check --no-first-run
