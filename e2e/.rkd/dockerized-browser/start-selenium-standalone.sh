9ca047e8b6b42188dcd50e9e795ce865fe60bdd3
Andrew Johnson <blackandred@users.noreply.github.com>
2021-02-26 09:37:34 +0100

    E2E: Show about:blank instead of chrome animation. The chrome animation is taking 300% CPU

--- e2e/.rkd/dockerized-browser/start-selenium-standalone.sh
+++ e2e/.rkd/dockerized-browser/start-selenium-standalone.sh
@@ -11,4 +11,4 @@ sudo apt-get update
 sudo apt-get install socat -y
 socat tcp4-listen:9223,reuseaddr,fork,bind=0.0.0.0 tcp:127.0.0.1:9222 &
 
-exec google-chrome --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --no-default-browser-check --no-first-run
+exec google-chrome --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --no-default-browser-check --no-first-run about:blank
