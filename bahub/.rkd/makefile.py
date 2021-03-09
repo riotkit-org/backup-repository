57cff58e78644ac4b3052701cbfc5abe3b16962a
Andrew Johnson <blackandred@users.noreply.github.com>
2021-03-09 07:11:42 +0100

    #139: Add docker build for bahub

--- bahub/.rkd/makefile.py
+++ bahub/.rkd/makefile.py
@@ -6,12 +6,12 @@ from rkd_python import imports as PythonBuildTasksImports
 IMPORTS = PythonBuildTasksImports()
 
 TASKS = [
-    Task(':release:docker-image', [':sh', '-c', ''' set -x
-        cd ../ && sudo docker build . -f bahub/.rkd/docker/Dockerfile -t quay.io/riotkit/bahub:latest
+    Task(':build:docker', [':sh', '-c', ''' set -x
+        cd ../ && sudo docker build . -f bahub/.rkd/docker/Dockerfile -t quay.io/riotkit/bahub:latest-dev
     ''']),
 
-    Task(':run:docker', [':sh', '-c', ''' set -x
-        docker run --rm --name bahub -e CONFIG=bahub.conf.yaml quay.io/riotkit/bahub:latest
+    Task(':release:docker', [':sh', '-c', ''' set -x
+        cd ../ && docker push quay.io/riotkit/bahub:latest-dev
     ''']),
 
     Task(':test:unit', [':py:unittest'])
