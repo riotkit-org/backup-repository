34c1c744eed4e42c90c86058bf5d8db90d7112ab
Andrew Johnson <blackandred@users.noreply.github.com>
2021-03-13 23:57:40 +0100

    #139: Unify RKD tasks - :release:docker

--- bahub/.rkd/makefile.py
+++ bahub/.rkd/makefile.py
@@ -1,18 +1,43 @@
-
-from rkd.api.syntax import TaskAliasDeclaration as Task
+import re
+from rkd.api.contract import ExecutionContext, TaskInterface, ArgparseArgument
+from rkd.api.syntax import TaskAliasDeclaration as Pipeline, TaskDeclaration as Task
+from rkd.standardlib.core import CallableTask
 from rkd_python import imports as PythonBuildTasksImports
 
 
-IMPORTS = PythonBuildTasksImports()
+def release_docker(ctx: ExecutionContext, task: TaskInterface) -> bool:
+    tag = ctx.get_arg('ref')
+    should_push = ctx.get_arg('--push')
+
+    if should_push:
+        task.sh('docker push quay.io/riotkit/bahub:latest-dev')
+
+    if re.match('^v([0-9.\-A-Z]+)$', tag):
+        task.sh('docker tag quay.io/riotkit/bahub:latest-dev quay.io/riotkit/bahub:%s' % tag)
+        task.rkd(
+            ['@', '--image', 'quay.io/riotkit/bahub:%s' % tag, '--propagate', ':docker:tag']
+            + ([':docker:push'] if should_push else [])
+        )
+
+        task.sh('docker tag quay.io/riotkit/bahub:latest-dev quay.io/riotkit/bahub:latest-release')
+        task.sh('docker push quay.io/riotkit/bahub:latest-release')
+
+        return True
+
+    return True
+
+
+IMPORTS = PythonBuildTasksImports() + [
+    Task(CallableTask(':release:docker', callback=release_docker, argparse_options=[
+        ArgparseArgument(['ref'], {'help': 'Tag name, or latest'}),
+        ArgparseArgument(['--push'], {'help': 'Should push?', 'action': 'store_true'})
+    ]))
+]
 
 TASKS = [
-    Task(':build:docker', [':sh', '-c', ''' set -x
+    Pipeline(':build:docker', [':sh', '-c', ''' set -x
         cd ../ && sudo docker build . -f bahub/.rkd/docker/Dockerfile -t quay.io/riotkit/bahub:latest-dev
     ''']),
 
-    Task(':release:docker', [':sh', '-c', ''' set -x
-        cd ../ && docker push quay.io/riotkit/bahub:latest-dev
-    ''']),
-
-    Task(':test:unit', [':py:unittest'])
+    Pipeline(':test:unit', [':py:unittest'])
 ]
