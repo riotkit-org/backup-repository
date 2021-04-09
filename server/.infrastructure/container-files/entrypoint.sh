a4f91e637672819b84fb458371cbe1916736655c
Andrew Johnson <blackandred@users.noreply.github.com>
2021-04-09 08:04:38 +0200

    Server: Fix issue - NGINX and PHP-FPM configuration files were not rendered due to not executed entrypoint. Fixed with this commit + https://github.com/riotkit-org/docker-php-app/commit/7b25bcf6487323b708399e03fe5c36eadea2ba77

--- server/.infrastructure/container-files/entrypoint.sh
+++ server/.infrastructure/container-files/entrypoint.sh
@@ -95,4 +95,5 @@ touch "${appLogPath}"
 touch "/var/log/nginx/error.log"
 touch "/var/log/nginx/access.log"
 
-exec multirun -v "nginx" "php-fpm -F -O" "tail -f ${appLogPath}" "tail -f /var/log/nginx/error.log" "tail -f /var/log/nginx/access.log"
+# execute original docker-php-app's (base image) entrypoint
+exec /entrypoint.sh multirun -v "nginx" "php-fpm -F -O" "tail -f ${appLogPath}" "tail -f /var/log/nginx/error.log" "tail -f /var/log/nginx/access.log"
