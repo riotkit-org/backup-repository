79cdc325769a199b49c27a6ca6b951bc6fdbcd1c
Andrew Johnson <blackandred@users.noreply.github.com>
2021-04-30 21:52:53 +0200

    Bahub: #138 implemented correct procedure for PostgreSQL restore - database needs to be shutdown before

--- bahub/bahub/adapters/postgres_dump.py
+++ bahub/bahub/adapters/postgres_dump.py
@@ -2,6 +2,8 @@
 PostgreSQL Adapter
 ==================
 """
+from typing import List
+
 from rkd.api.inputoutput import IO
 from ..model import BackupDefinition
 from ..inputoutput import StreamableBuffer
@@ -78,12 +80,15 @@ class Definition(BackupDefinition):
         :return:
         """
 
-        parameters = '-h {host} -U {user} '.format(host=self._spec['host'], user=self._spec['user'])
+        parameters = ' -h {host} -U {user} '.format(host=self._spec['host'], user=self._spec['user'])
 
         if self._spec.get('port'):
             parameters += ' -p {port} '.format(port=str(self._spec.get('port')))
 
         return parameters
+
+    def is_dumping_all_databases(self):
+        return not self._spec.get('database')
 
     def get_dump_command(self) -> str:
         """
@@ -91,15 +96,15 @@ class Definition(BackupDefinition):
         :return:
         """
 
-        parameters = 'pg_dump' if self._spec.get('database') else 'pg_dumpall'
-
+        parameters = 'pg_dumpall' if self.is_dumping_all_databases() else 'pg_dump'
         parameters += self._get_common_parameters()
-        parameters += ' --set ON_ERROR_STOP=on '
+        parameters += ' --clean '
+
+        if not self.is_dumping_all_databases():
+            parameters += ' --set ON_ERROR_STOP=on '
 
         if self._spec.get('database'):
             parameters += ' {database} '.format(database=self._spec.get('database'))
-        else:
-            parameters += ' --all-databases '
 
         return parameters
 
@@ -116,6 +121,9 @@ class Definition(BackupDefinition):
 
         return parameters
 
+    def get_psql_command(self) -> str:
+        return 'psql --tuples-only ' + self._get_common_parameters()
+
     def get_password(self) -> str:
         return self._spec.get('password')
 
@@ -127,6 +135,32 @@ class Adapter(AdapterInterface):
 
     Understands how to backup & restore running PostgreSQL databases using pg_dump/pg_dumpall and psql basic tools
     """
+
+    def terminate_all_connections(self, definition: Definition):
+        self.psql(
+            sql='SELECT pg_terminate_backend(pid) FROM pg_stat_activity  WHERE pid <> pg_backend_pid()',
+            definition=definition
+        )
+
+    def limit_connections_to_database(self, definition: Definition, db_name: str, limit: int):
+        self.psql(
+            sql='ALTER DATABASE ' + db_name + ' CONNECTION LIMIT ' + str(limit) + ';',
+            definition=definition
+        )
+
+    def psql(self, sql: str, definition: Definition):
+        return definition.transport()\
+            .capture('echo "' + sql + '" | ' + definition.get_psql_command())
+
+    def list_all_databases(self, definition: Definition) -> List[str]:
+        output = self.psql('SELECT datname FROM pg_database WHERE datistemplate = false;', definition)
+
+        return list(
+            filter(
+                lambda x: x,
+                map(lambda part: part.strip().decode('utf-8'), output.split(b"\n"))
+            )
+        )
 
     def backup(self, definition: Definition) -> StreamableBuffer:
         """
@@ -143,6 +177,14 @@ class Adapter(AdapterInterface):
         )
 
     def restore(self, definition: Definition, in_buffer: StreamableBuffer, io: IO) -> None:
+        io.info('Terminating all database connections')
+        self.terminate_all_connections(definition)
+        dbs = self.list_all_databases(definition)
+
+        for db in dbs:
+            io.info('Setting connection limit=0 for database {}'.format(db))
+            self.limit_connections_to_database(definition, db, limit=0)
+
         restore_process = definition.transport()\
             .buffered_execute(
                 definition.get_restore_command(),
@@ -150,7 +192,13 @@ class Adapter(AdapterInterface):
                 env={'PGPASSWORD': definition.get_password()}
         )
 
-        self._read_from_restore_process(restore_process, io)
+        try:
+            io.info('Performing restore using psql')
+            self._read_from_restore_process(restore_process, io)
+        finally:
+            for db in dbs:
+                io.info('Setting connection limit=unlimited for database {}'.format(db))
+                self.limit_connections_to_database(definition, db, limit=-1)
 
     @staticmethod
     def create_definition(config: dict, name: str) -> Definition:
