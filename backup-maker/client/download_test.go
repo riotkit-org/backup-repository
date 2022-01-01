0b5274de6956f26fd83c5c99c3a3e6f1e33b4031
Andrew Johnson <blackandred@users.noreply.github.com>
2022-01-01 21:57:37 +0100

    #161: Increase coverage for HTTP/shell layer in `download` and `restore`

--- backup-maker/client/download_test.go
+++ backup-maker/client/download_test.go
@@ -10,6 +10,15 @@ import (
 	"testing"
 )
 
+func createExampleContext() context.ActionContext {
+	ctx := context.ActionContext{}
+	ctx.Timeout = 5
+	ctx.VersionToRestore = "v1"
+	ctx.Url = "http://localhost"
+
+	return ctx
+}
+
 // TestDownload checks most basic case: No GPG encryption, no connection issues, HTTP 200 OK response
 func TestDownload(t *testing.T) {
 	client := new(HttpClientMock)
@@ -19,13 +28,38 @@ func TestDownload(t *testing.T) {
 	}
 
 	// perform action
-	ctx := context.ActionContext{}
-	ctx.Timeout = 5
-	ctx.VersionToRestore = "v1"
-	ctx.Url = "http://localhost"
+	ctx := createExampleContext()
 	_ = DownloadBackupIntoProcessStdin(ctx, "cat - > /tmp/test-data", client)
 
 	// verify result - there should be a valid content fetched from HTTP response into a file
 	data, _ := ioutil.ReadFile("/tmp/test-data")
 	assert.Equal(t, "SOME GPG ENCRYPTED CONTENT THERE", string(data))
+}
+
+func TestDownload_FailsWhenHTTPCodeIsNotSuccess(t *testing.T) {
+	client := new(HttpClientMock)
+	client.mockedResponse = http.Response{
+		StatusCode: 403,
+		Status:     "403 Not authorized",
+		Body:       io.NopCloser(strings.NewReader("Not authorized")),
+	}
+
+	ctx := createExampleContext()
+	err := DownloadBackupIntoProcessStdin(ctx, "cat - > /tmp/test-data", client)
+
+	assert.Equal(t, "Invalid request. Got 403 Not authorized as response", err.Error())
+}
+
+func TestDownload_FailsWhenShellCommandFails(t *testing.T) {
+	client := new(HttpClientMock)
+	client.mockedResponse = http.Response{
+		StatusCode: 200,
+		Body:       io.NopCloser(strings.NewReader("SOME GPG ENCRYPTED CONTENT THERE")),
+	}
+
+	// perform action
+	ctx := createExampleContext()
+	err := DownloadBackupIntoProcessStdin(ctx, "/bin/false", client)
+
+	assert.Equal(t, "exit status 1", err.Error())
 }
