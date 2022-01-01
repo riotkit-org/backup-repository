2b8192e552abb9aa767acb6f497d5ffd0f4ec5e5
Andrew Johnson <blackandred@users.noreply.github.com>
2022-01-01 22:26:50 +0100

    #161: Increase coverage for HTTP/shell layer in `download` and `restore`

--- backup-maker/client/download_test.go
+++ backup-maker/client/download_test.go
@@ -2,10 +2,12 @@ package client
 
 import (
 	"github.com/riotkit-org/backup-repository/backup-maker/context"
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 	"testing"
 )
@@ -15,8 +17,18 @@ func createExampleContext() context.ActionContext {
 	ctx.Timeout = 5
 	ctx.VersionToRestore = "v1"
 	ctx.Url = "http://localhost"
+	ctx.LogLevel = 5
+	log.SetLevel(log.Level(ctx.LogLevel))
 
 	return ctx
+}
+
+func prepareBuildDirectory() {
+	_ = os.Mkdir("../.build", os.FileMode(0755))
+}
+
+func cleanUpFileIfExists(path string) {
+	_ = os.Remove(path)
 }
 
 // TestDownload checks most basic case: No GPG encryption, no connection issues, HTTP 200 OK response
@@ -62,4 +74,27 @@ func TestDownload_FailsWhenShellCommandFails(t *testing.T) {
 	err := DownloadBackupIntoProcessStdin(ctx, "/bin/false", client)
 
 	assert.Equal(t, "exit status 1", err.Error())
+}
+
+func TestDownload_SuccessWithValidGPG(t *testing.T) {
+	prepareBuildDirectory()
+	cleanUpFileIfExists("../.build/TestDownload_SuccessWithValidGPG")
+
+	// mock HTTP response, so it will return GPG encrypted file (main.go)
+	testFile, _ := ioutil.ReadFile("../resources/test/example-encrypted-file")
+	client := new(HttpClientMock)
+	client.mockedResponse = http.Response{
+		StatusCode: 200,
+		Body:       io.NopCloser(strings.NewReader(string(testFile))),
+	}
+
+	ctx := createExampleContext()
+	ctx.ActionType = "download"
+	ctx.Gpg, _ = context.CreateGPGContext("", "../resources/test/gpg-key.asc", "riotkit", "test@riotkit.org", true)
+	defer ctx.Gpg.CleanUp()
+
+	_ = DownloadBackupIntoProcessStdin(ctx, "cat - > ../.build/TestDownload_SuccessWithValidGPG", client)
+
+	decryptedResult, _ := ioutil.ReadFile("../.build/TestDownload_SuccessWithValidGPG")
+	assert.Contains(t, string(decryptedResult), "package main") // the encrypted file is the main.go file
 }
