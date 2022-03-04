907a4e178db7ed71ea2acb52f44fbd34ba9179ab
blackandred <22807686+blackandred@users.noreply.github.com>
2022-03-04 16:36:56 +0100

    ci: Add missing CRD
    /test skip /pkg skip

--- k8s.mk
+++ k8s.mk
@@ -8,4 +8,21 @@ k8s_minio:
 	helm upgrade --install minio minio/minio -n backup-repository --values ./helm/examples/minio.values.yaml --wait --timeout 2m0s
 
 k8s_test_backup_repository:
-	helm upgrade --install backup-repository ./helm/backup-repository-server -n backup-repository --values ./helm/examples/backup-repository-ci.values.yaml
+	helm upgrade --install backup-repository ./helm/backup-repository-server -n backup-repository --values ./helm/examples/backup-repository-ci.values.yaml --wait --timeout 30s
+
+k8s_crd:
+	kubectl apply -f crd
+
+k8s_install: k8s_postgres k8s_minio k8s_crd k8s_test_backup_repository
+
+k8s_test:
+	sleep 10
+	kubectl get pods -A
+	kubectl get events -A
+	kubectl logs deployment/backup-repository-backup-repository-server -n backup-repository
+
+	curl -vvv -k http://localhost:30080/health
+
+	if kubectl get events -A | grep "backup-repository" | grep "Failed" 2>&1 > /dev/null; then
+	    exit 1
+	fi
