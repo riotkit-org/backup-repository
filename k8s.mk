39c995ed176b19adf59be7d679ee6e3f31b51952
blackandred <22807686+blackandred@users.noreply.github.com>
2022-03-04 08:58:00 +0100

    ci: Wait for dependent services to get up
    /test skip /pkg skip

--- k8s.mk
+++ k8s.mk
@@ -1,11 +1,11 @@
 
 k8s_postgres:
 	helm repo add bitnami https://charts.bitnami.com/bitnami
-	helm upgrade --install postgres bitnami/postgresql -n backup-repository --create-namespace --values ./helm/examples/postgresql.values.yaml
+	helm upgrade --install postgres bitnami/postgresql -n backup-repository --create-namespace --values ./helm/examples/postgresql.values.yaml --wait --timeout 2m0s
 
 k8s_minio:
 	helm repo add minio https://helm.min.io/
-	helm upgrade --install minio minio/minio -n backup-repository --values ./helm/examples/minio.values.yaml
+	helm upgrade --install minio minio/minio -n backup-repository --values ./helm/examples/minio.values.yaml --wait --timeout 2m0s
 
 k8s_test_backup_repository:
 	helm install backup-repository ./helm/backup-repository-server -n backup-repository --values ./helm/examples/backup-repository-ci.values.yaml
