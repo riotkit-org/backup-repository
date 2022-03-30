f87ec2a5d525fa157c9b6971d2ab161000b7235b
blackandred <22807686+blackandred@users.noreply.github.com>
2022-03-30 21:42:25 +0200

    refactor: Separate makefiles

--- test.mk
+++ test.mk
@@ -1,20 +1,9 @@
-include k8s.mk
-include test.mk
+#
+# Common tasks
+#
 
-all: build run
-
-test:
-	go test -v ./...
-
-setup_api_tests:
-	kubectl apply -f "docs/examples/" -n backup-repository
-	pipenv install
-
-api_tests:
-	pipenv run pytest -s
-
-coverage:
-	go test -v ./... -covermode=count -coverprofile=coverage.out
+SERVER_PORT=8080
+SERVER_URL=http://localhost:${SERVER_PORT}
 
 test_health:
 	curl -s -X GET '${SERVER_URL}/health'
@@ -67,53 +56,3 @@ test_collection_health:
 
 test_list_versions:
 	curl -s -X GET -H 'Authorization: Bearer ${TOKEN}' '${SERVER_URL}/api/stable/repository/collection/iwa-ait/version'
-
-postgres:
-	docker run -d \
-        --name br_postgres \
-        -e POSTGRES_PASSWORD=postgres \
-        -e POSTGRES_USER=postgres \
-        -e POSTGRES_DB=postgres \
-        -e PGDATA=/var/lib/postgresql/data/pgdata \
-        -v $$(pwd)/.build/postgres:/var/lib/postgresql \
-        -p 5432:5432 \
-        postgres:14.1-alpine
-
-postgres_refresh:
-	docker rm -f br_postgres || true
-	sudo rm -rf $$(pwd)/.build/postgres
-	make postgres
-
-minio:
-	docker run -d \
-		--name br_minio \
-		-p 9000:9000 \
-	    -p 9001:9001 \
-	    -v $$(pwd)/.build/minio:/data \
-	    -e "MINIO_ROOT_USER=AKIAIOSFODNN7EXAMPLE" \
-	    -e "MINIO_ROOT_PASSWORD=wJaFuCKtnFEMI/CApItaliSM/bPxRfiCYEXAMPLEKEY" \
-		quay.io/minio/minio:RELEASE.2022-02-16T00-35-27Z server /data --console-address 0.0.0.0:9001
-
-run:
-	export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE; \
-	export AWS_SECRET_ACCESS_KEY=wJaFuCKtnFEMI/CApItaliSM/bPxRfiCYEXAMPLEKEY; \
-	\
-	./.build/backup-repository \
-		--db-password=postgres \
-		--db-user=postgres \
-		--db-password=postgres \
-		--db-name=postgres \
-		--health-check-key=changeme \
-		--jwt-secret-key="secret key" \
-		--storage-io-timeout="5m" \
-		--listen=":${SERVER_PORT}" \
-		--storage-url="s3://mybucket?endpoint=localhost:9000&disableSSL=true&s3ForcePathStyle=true&region=eu-central-1"
-
-build:
-	CGO_ENABLED=0 GO111MODULE=on go build -tags=nomsgpack -o ./.build/backup-repository
-
-lint:
-	export GO111MODULE=on; \
-	golangci-lint run \
-		--verbose \
-		--build-tags build
