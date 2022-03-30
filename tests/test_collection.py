c289a18ace0116db12433736e8351008c2538e7c
blackandred <22807686+blackandred@users.noreply.github.com>
2022-03-30 21:44:13 +0200

    tests: Fix tests after recent API change to distinct alpha from stable apis

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -4,12 +4,12 @@ from tests import BaseTestCase
 class CollectionTest(BaseTestCase):
     def test_cannot_upload_if_uploader_role_not_present(self):
         token = self.login('unprivileged', 'admin')
-        response = self.post("/api/stable/repository/collection/iwa-ait/version", "something", auth=f'Bearer {token}')
+        response = self.post("/api/alpha/repository/collection/iwa-ait/version", "something", auth=f'Bearer {token}')
 
         assert "not authorized to upload versions to this collection" in str(response.content)
 
     def test_gpg_detection(self):
         token = self.login('admin', 'admin')
-        response = self.post("/api/stable/repository/collection/iwa-ait/version", "hello this is not a gpg data", auth=f'Bearer {token}')
+        response = self.post("/api/alpha/repository/collection/iwa-ait/version", "hello this is not a gpg data", auth=f'Bearer {token}')
 
         assert "cannot upload version. cannot upload file, cannot copy stream, error: first chunk of uploaded data does not contain a valid GPG header" in str(response.content)
