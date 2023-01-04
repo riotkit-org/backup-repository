30ecf86902ee8a1761cf94b24e15daa86f33d61d
B&R <example@example.org>
2023-01-04 12:32:24 +0100

    fix: Labels - adjust to the skaffold related changes

--- tests/test_health.py
+++ tests/test_health.py
@@ -12,25 +12,25 @@ class HealthTest(BaseTestCase):
 
     def test_readiness_when_postgresql_is_off(self):
         try:
-            self.scale("sts", "postgres-postgresql", 0)
-            self.wait_for("app.kubernetes.io/instance=postgres", ready=False)
+            self.scale("sts", "postgresql", 0, ns="db")
+            self.wait_for("app.kubernetes.io/instance=postgresql", ready=False, ns="db")
             response = self.get("/ready?code=changeme", auth=None)
 
             assert response.status_code >= 500, response.content
         finally:
-            self.scale("sts", "postgres-postgresql", 1)
-            self.wait_for("app.kubernetes.io/instance=postgres", ready=True)
+            self.scale("sts", "postgresql", 1, ns="db")
+            self.wait_for("app.kubernetes.io/instance=postgresql", ready=True, ns="db")
 
     def test_readiness_when_storage_is_off(self):
         try:
-            self.scale("deployment", "minio", 0)
-            self.wait_for("app=minio", ready=False)
+            self.scale("deployment", "minio", 0, ns="storage")
+            self.wait_for("app=minio", ready=False, ns="storage")
             response = self.get("/ready?code=changeme", auth=None)
 
             assert response.status_code >= 500, response.content
         finally:
-            self.scale("deployment", "minio", 1)
-            self.wait_for("app=minio", ready=True)
+            self.scale("deployment", "minio", 1, ns="storage")
+            self.wait_for("app=minio", ready=True, ns="storage")
 
     def test_kubernetes_connection_will_be_degraded_if_crds_not_present(self):
         try:
